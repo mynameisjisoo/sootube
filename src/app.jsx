ab253e43678d82d2ee3ba35b7de0fb746a6c7d89
jisoo <devjisoolee@gmail.com>
2021-10-21 11:35:56 +0900

    youtube API fetch success

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello</h1>;
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }; //request option 전달하는 것
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDrRNxpD150VNnS7ElMn8Q0Yv0ZgoERa_c',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []); //마운트 되었을 때만 호출
+  return <></>;
 }
 
 export default App;
