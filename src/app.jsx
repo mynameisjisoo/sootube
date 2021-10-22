0007f1f8b0f8e53ecff522f04e72683eb62045fd
jisoo <devjisoolee@gmail.com>
2021-10-23 01:14:15 +0900

    Add video player

--- src/app.jsx
+++ src/app.jsx
@@ -3,24 +3,27 @@ import styles from './app.module.css';
 
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
+import VideoPlayer from './components/video_play/video_player';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
     youtube
       .search(query) //
-      .then(videos => setVideos(videos));
+      .then(videos => setVideos(videos))
+      .catch(error => console.log('error', error)); //Error처리는 서비스 API를 소비하는 쪽에서 하는것이 좋음
   };
 
   useEffect(() => {
     youtube //
       .mostPopular() //
-      .then(videos => setVideos(videos));
+      .then(videos => setVideos(videos))
+      .catch(error => console.log('error', error));
   }, []); //마운트 되었을 때만 호출
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <VideoList className={styles.videoList} videos={videos} />
     </div>
   );
 }
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import VideoItem from '../video_item/video_item';
+import VideoPlayer from '../video_play/video_player';
 import styles from './video_list.module.css';
 
 const VideoList = props => {
+  const [src, setSrc] = useState([]);
+
+  const handleSrc = videoSrc => {
+    setSrc(videoSrc);
+  };
   return (
-    <ul className={styles.videos}>
-      {props.videos.map(video => (
-        <VideoItem video={video} key={video.id} />
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <div className={styles.videoPlayer}>
+        <VideoPlayer src={src} />
+      </div>
+      <ul className={styles.videos}>
+        {props.videos.map(video => (
+          <VideoItem video={video} key={video.id} handleSrc={handleSrc} />
+        ))}
+      </ul>
+    </div>
   );
 };
 export default VideoList;
